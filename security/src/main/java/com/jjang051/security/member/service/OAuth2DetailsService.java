57ecb72a2e9628cddfda4fd21e707f1ec6c7792b
jjang051 <tjjjang051@nate.com>
2026-06-30 11:58:52 +0900

    소셜 로그인

--- security/src/main/java/com/jjang051/security/member/service/OAuth2DetailsService.java
+++ security/src/main/java/com/jjang051/security/member/service/OAuth2DetailsService.java
@@ -7,12 +7,18 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 @Slf4j
 public class OAuth2DetailsService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         log.info("소셜로그인시도하면 여기로 들어온다.");
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        log.info("userRequest = {}", userRequest);
+        Map<String,Object> oauth2UserInfo = (Map)oAuth2User.getAttributes();
+        log.info("oauth2UserInfo = {}", oauth2UserInfo);
         return super.loadUser(userRequest);
     }
 }

c4a7064dff45d58eeb3d2c3ddd21e91fb0832881
jjang051 <tjjjang051@nate.com>
2026-07-02 12:01:13 +0900

    vote

--- security/src/main/java/com/jjang051/security/vote/controller/VoteController.java
+++ security/src/main/java/com/jjang051/security/vote/controller/VoteController.java
@@ -4,16 +4,17 @@ import com.jjang051.security.vote.dao.VoteDao;
 import com.jjang051.security.vote.dto.VoteDto;
 import com.jjang051.security.vote.service.VoteService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/vote")
 @RequiredArgsConstructor
+@Slf4j
 public class VoteController {
     private final VoteService voteService;
 
@@ -23,4 +24,12 @@ public class VoteController {
         model.addAttribute("voteDtoList",voteDtoList);
         return "vote/list";
     }
+    @GetMapping("/{voteNo}")
+    @ResponseBody
+    public String detail(@PathVariable int voteNo, @RequestParam(name="voteNo") int voteNoParam, Model model){
+        log.info("voteNo = {}", voteNo);
+        log.info("voteNoParam = {}", voteNoParam);
+        return "vote/detail";
+    }
+
 }

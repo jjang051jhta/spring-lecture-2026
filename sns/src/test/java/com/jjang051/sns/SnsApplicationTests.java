af7cf4ad4155ed79c662ade869b841e75204dd88
jjang051 <tjjjang051@nate.com>
2026-08-18 11:26:55 +0900

    sns

---
+++ sns/src/test/java/com/jjang051/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjang051.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

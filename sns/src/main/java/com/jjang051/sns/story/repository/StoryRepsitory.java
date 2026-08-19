99c4edac158827646267a89b8296e014dd014578
jjang051 <tjjjang051@nate.com>
2026-08-19 10:22:09 +0900

    sns

--- sns/src/main/java/com/jjang051/sns/story/repository/StoryRepsitory.java
+++ sns/src/main/java/com/jjang051/sns/story/repository/StoryRepsitory.java
@@ -3,6 +3,8 @@ package com.jjang051.sns.story.repository;
 
 import com.jjang051.sns.story.entity.Story;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,7 +13,16 @@ public interface StoryRepsitory extends JpaRepository<Story, Long> {
     //jpa가 알아서 만들어 준다.
     //proxy
     //query method
-    List<Story> findAllByOrderByIdDesc();
+    List<Story> findAllByOrderByIdDesc(); //jpa는 구현이 없다.  스프링이 proxy를 만들어서 주입해 준다.
+
+    //Story는 entity이다. 대소문자 잘 맞춰서 써야 한다.
+    //jpql
+    //@Query(value="SELECT * FROM Story s ORDER BY id DESC", nativeQuery = true)
+    @Query("SELECT s FROM Story s ORDER BY s.id DESC")
+    List<Story> customFindAllByOrderByIdDesc();
 
     List<Story> findAllByWriter(String writer);
+
+    @Query("SELECT s FROM Story s WHERE s.writer = :writer")
+    List<Story> customFindAllByWriter(@Param("writer") String writer);
 }

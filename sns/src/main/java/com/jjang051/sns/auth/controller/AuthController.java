3869dafd85ca55a21b78a940dc101ee4d127f993
jjang051 <tjjjang051@nate.com>
2026-08-21 11:09:53 +0900

    sns

--- sns/src/main/java/com/jjang051/sns/auth/controller/AuthController.java
+++ sns/src/main/java/com/jjang051/sns/auth/controller/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -32,17 +33,24 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<ApiResponseDto<LoginResponseDto>> login(@RequestBody LoginDto loginDto) {
         log.info("login");
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginDto.getUserId(), loginDto.getUserPassword())
-        );
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginDto.getUserId(), loginDto.getUserPassword())
+            );
 
-        String accessToken = jwtProvider.createAccessToken(authentication.getName());
-        LoginResponseDto loginResponseDto = new LoginResponseDto(accessToken);
-        return ResponseEntity.status(HttpStatus.OK)
-                             .body(
-                                     new ApiResponseDto<>(200,"login success", loginResponseDto)
-                             );
+            String accessToken = jwtProvider.createAccessToken(authentication.getName());
+            LoginResponseDto loginResponseDto = new LoginResponseDto(accessToken);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(
+                            new ApiResponseDto<>(200, "login success", loginResponseDto)
+                    );
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(
+                            new ApiResponseDto<>(401, "아이디 또는 비밀번호가 맞지 않습니다.", null)
+                    );
+        }
     }
     @GetMapping("/me")
     public ResponseEntity<ApiResponseDto<MeResponseDto>> getMe(Authentication authentication) {
